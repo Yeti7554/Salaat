c05ff1b8724478b9ea40842f302a74e58f9c58a0
Yeti7554clear <aasiddiq@ualberta.ca>
2023-04-03 20:36:45 -0600

    Added html css and js, used api to call prayer times for Edmonton only

---
+++ script.js
@@ -0,0 +1,28 @@
+async function getPrayerTimes() {
+  const url = 'https://api.aladhan.com/v1/timingsByCity?city=Edmonton&country=Canada&method=2';
+  
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const timings = data.data.timings;
+    
+    const fajr = timings.Fajr;
+    const dhuhr = timings.Dhuhr;
+    const asr = timings.Asr;
+    const maghrib = timings.Maghrib;
+    const isha = timings.Isha;
+    
+    const prayerTimes = [fajr, dhuhr, asr, maghrib, isha];
+    
+    const prayerTimeElements = document.querySelectorAll('table tr:not(:first-child) td:last-child');
+    prayerTimeElements.forEach((element, index) => {
+      element.textContent = prayerTimes[index];
+    });
+    
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getPrayerTimes();
