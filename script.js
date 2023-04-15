48bbc980a214020db1c2fcbd3e664ea97a582ce5
Yeti7554clear <aasiddiq@ualberta.ca>
2023-04-14 19:37:08 -0600

    find the times for edmonton only

--- script.js
+++ script.js
@@ -34,32 +34,34 @@ getUserCity()
     console.error("Error getting user city:", error);
   });
 
-  async function getPrayerTimes() {
-    const url = `https://api.aladhan.com/v1/timingsByCity?city=${userCity}&country=Canada&method=1`;
+
+  
+async function getPrayerTimes() {
+  const url = `https://api.aladhan.com/v1/timingsByCity?city=Edmonton&country=Canada&method=1`;
+  
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    const timings = data.data.timings;
     
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-  
-      const timings = data.data.timings;
-      
-      const fajr = timings.Fajr;
-      const dhuhr = timings.Dhuhr;
-      const asr = timings.Asr;
-      const maghrib = timings.Maghrib;
-      const isha = timings.Isha;
-      
-      const prayerTimes = [fajr, dhuhr, asr, maghrib, isha];
-      
-      const prayerTimeElements = document.querySelectorAll('table tr:not(:first-child) td:last-child');
-      prayerTimeElements.forEach((element, index) => {
-        element.textContent = prayerTimes[index];
-      });
-      
-    } catch (error) {
-      console.error(error);
-    }
+    const fajr = timings.Fajr;
+    const dhuhr = timings.Dhuhr;
+    const asr = timings.Asr;
+    const maghrib = timings.Maghrib;
+    const isha = timings.Isha;
+    
+    const prayerTimes = [fajr, dhuhr, asr, maghrib, isha];
+    
+    const prayerTimeElements = document.querySelectorAll('table tr:not(:first-child) td:last-child');
+    prayerTimeElements.forEach((element, index) => {
+      element.textContent = prayerTimes[index];
+    });
+    
+  } catch (error) {
+    console.error(error);
   }
-  
-  getPrayerTimes();
+}
+
+getPrayerTimes();
 
